14a5744aade4655c23f515a0e5a8f58c18861d97
Kalam889 <mdkalam1905@gmail.com>
2025-09-16 18:12:32 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+function press(val) {
+  display.value += val;
+}
+
+function ClearDisplay() {
+  display.value = "";
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
